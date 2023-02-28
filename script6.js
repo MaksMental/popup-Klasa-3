a67d92850fe5a97f89c106c5d70d3e89feb59c23
MaksMental <116297987+MaksMental@users.noreply.github.com>
2023-02-28 06:18:43 +0100

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,250 @@
+
+function klikBourbon(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup1');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+
+function klikBourbon2(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup2');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+
+function klikBourbon3(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup3');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+
+function klikBourbon4(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup4');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+
+function klikBourbon5(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup5');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+
+function klikBourbon6(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup6');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon7(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup7');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon8(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup8');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon9(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup9');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon10(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup10');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon11(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup11');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon12(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup12');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+
+function klikBourbon13(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup13');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon14(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup14');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon15(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup15');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon16(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup16');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon17(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup17');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon18(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup18');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon19(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup19');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function klikBourbon20(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup20');
+    popup.classList.toggle('active')
+    let odglos = new Audio("Cafe_Belga.mp3");
+    odglos.play();
+}
+function przepis1(){
+    setTimeout("location.href='przepis1.html';",500);
+
+}
+function przepis2(){
+    setTimeout("location.href='przepis2.html';",500);
+
+}
+function przepis3(){
+    setTimeout("location.href='przepis3.html';",500);
+
+}
+function przepis4(){
+    setTimeout("location.href='przepis4.html';",500);
+
+}
+function przepis5(){
+    setTimeout("location.href='przepis5.html';",500);
+
+}
+function przepis6(){
+    setTimeout("location.href='przepis6.html';",500);
+
+}
+function przepis7(){
+    setTimeout("location.href='przepis7.html';",500);
+
+}
+function przepis8(){
+    setTimeout("location.href='przepis8.html';",500);
+
+}
+function przepis9(){
+    setTimeout("location.href='przepis9.html';",500);
+
+}
+function przepis10(){
+    setTimeout("location.href='przepis10.html';",500);
+
+}
+function przepis11(){
+    setTimeout("location.href='przepis11.html';",500);
+
+}
+function przepis12(){
+    setTimeout("location.href='przepis12.html';",500);
+
+}
+function przepis13(){
+    setTimeout("location.href='przepis13.html';",500);
+
+}
+function przepis14(){
+    setTimeout("location.href='przepis14.html';",500);
+
+}
+function przepis15(){
+    setTimeout("location.href='przepis15.html';",500);
+
+}
+function przepis16(){
+    setTimeout("location.href='przepis16.html';",500);
+
+}
+function przepis17(){
+    setTimeout("location.href='przepis17.html';",500);
+
+}
+function przepis18(){
+    setTimeout("location.href='przepis18.html';",500);
+
+}
+function przepis19(){
+    setTimeout("location.href='przepis19.html';",500);
+
+}
+function przepis20(){
+    setTimeout("location.href='przepis20.html';",500);
+
+}
+function powrot(){
+    setTimeout("location.href='index.html#site2';",500);
+}
